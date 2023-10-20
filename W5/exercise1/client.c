51edc0458135698c00d90cdc92fffcbc93e97848
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-20 13:16:41 +0700

    Update client.c

--- W5/exercise1/client.c
+++ W5/exercise1/client.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 
 #define MAXLINE 1024
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 3) {
         printf("Usage: %s IP_Addr Port_Number\n", argv[0]);
         exit(1);
@@ -16,8 +16,7 @@ int main(int argc, char *argv[]) {
     char buffer[MAXLINE];
     struct sockaddr_in servaddr;
 
-    int port = atoi(argv[2]);
-
+    // Create socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
@@ -25,39 +24,35 @@ int main(int argc, char *argv[]) {
 
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
+    servaddr.sin_port = htons(atoi(argv[2]));
 
     if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
         perror("inet_pton");
         exit(1);
     }
 
+    // Connect to the server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
         perror("connect");
         exit(1);
     }
 
     while (1) {
-        printf("Enter a string (blank to exit): ");
+        printf("Send to server: ");
         if (fgets(buffer, MAXLINE, stdin) == NULL) {
-            break;
-        }
-
-        if (strlen(buffer) <= 1) {
             break;
         }
 
         send(sockfd, buffer, strlen(buffer), 0);
 
-        ssize_t n = recv(sockfd, buffer, MAXLINE, 0);
+        int n = recv(sockfd, buffer, MAXLINE, 0);
         if (n <= 0) {
             perror("recv");
             break;
         }
 
         buffer[n] = '\0';
-
-        printf("Received from server:\n%s", buffer);
+        printf("Received from server: %s", buffer);
     }
 
     close(sockfd);
