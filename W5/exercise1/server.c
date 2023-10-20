5383a3a16c823a15ebb1c2aa6f1aface2b0602cc
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-10-20 13:16:43 +0700

    Update server.c

--- W5/exercise1/server.c
+++ W5/exercise1/server.c
@@ -2,45 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 #define MAXLINE 1024
 
-void processString(char *input, char *alphaStr, char *digitStr) {
-    int alphaIdx = 0;
-    int digitIdx = 0;
+// Function to process the received string
+void processString(char* input, char* alphabet, char* digits) {
+    int alphaIndex = 0;
+    int digitIndex = 0;
 
     for (int i = 0; input[i] != '\0'; i++) {
         if (isalpha(input[i])) {
-            alphaStr[alphaIdx++] = input[i];
+            alphabet[alphaIndex++] = input[i];
         } else if (isdigit(input[i])) {
-            digitStr[digitIdx++] = input[i];
+            digits[digitIndex++] = input[i];
         } else {
-            // Handle error if non-alphabet and non-digit character is found
-            printf("Error: Non-alphabet and non-digit character found\n");
+            // Send an error message if a non-alphabet and non-digit character is found
+            strcpy(alphabet, "Error");
+            strcpy(digits, "Error");
             return;
         }
     }
-    alphaStr[alphaIdx] = '\0';
-    digitStr[digitIdx] = '\0';
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 2) {
         printf("Usage: %s Port_Number\n", argv[0]);
         exit(1);
     }
 
-    int listenfd, connfd;
-    char buffer[MAXLINE];
-    char alphaStr[MAXLINE];
-    char digitStr[MAXLINE];
-    struct sockaddr_in servaddr, client_addr;
-    socklen_t client_len;
-
     int port = atoi(argv[1]);
 
+    int listenfd, connfd;
+    char buffer[MAXLINE];
+    struct sockaddr_in servaddr;
+
+    // Create socket
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
@@ -51,11 +51,13 @@ int main(int argc, char *argv[]) {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(port);
 
+    // Bind socket
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
         perror("bind");
         exit(1);
     }
 
+    // Listen for incoming connections
     if (listen(listenfd, 5) == -1) {
         perror("listen");
         exit(1);
@@ -63,27 +65,30 @@ int main(int argc, char *argv[]) {
 
     printf("Server listening on port %d...\n", port);
 
-    client_len = sizeof(client_addr);
-    connfd = accept(listenfd, (struct sockaddr *)&client_addr, &client_len);
-
     while (1) {
-        ssize_t n = recv(connfd, buffer, MAXLINE, 0);
+        socklen_t client_len = sizeof(servaddr);
+        connfd = accept(listenfd, (struct sockaddr *)&servaddr, &client_len);
+
+        int n = recv(connfd, buffer, MAXLINE, 0);
         if (n <= 0) {
             perror("recv");
             break;
         }
 
         buffer[n] = '\0';
+        printf("Received from client: %s", buffer);
 
-        processString(buffer, alphaStr, digitStr);
+        char alphabet[MAXLINE];
+        char digits[MAXLINE];
 
-        if (strlen(alphaStr) > 0 || strlen(digitStr) > 0) {
-            send(connfd, alphaStr, strlen(alphaStr), 0);
-            send(connfd, digitStr, strlen(digitStr), 0);
-        }
+        processString(buffer, alphabet, digits);
+
+        send(connfd, alphabet, strlen(alphabet), 0);
+        send(connfd, digits, strlen(digits), 0);
+
+        close(connfd);
     }
 
-    close(connfd);
     close(listenfd);
 
     return 0;
