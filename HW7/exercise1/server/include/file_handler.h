7be8666a995d9e032f33cd823d48cd7ef9acf73c
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-07 23:33:33 +0700

    Update file_handler.h

--- HW7/exercise1/server/include/file_handler.h
+++ HW7/exercise1/server/include/file_handler.h
@@ -7,8 +7,8 @@
 #ifndef FILE_HANDLER_H
 #define FILE_HANDLER_H
 
-void delete(char * file);
-void save(char * data);
+void deleteFile(char * file);
+void saveFile(char * data);
 void encrypt(char * data, int shift);
 void decrypt(char * data, int shift);
 
