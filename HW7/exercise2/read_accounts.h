b21743dd3780ccb1ff5246a2d415dc036ad95f64
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-07 14:29:35 +0700

    Create read_accounts.h

---
+++ HW7/exercise2/read_accounts.h
@@ -0,0 +1,23 @@
+#ifndef READ_ACCOUNT_H
+#define READ_ACCOUNT_H
+
+// Define the structure for user information
+typedef struct UserInfo_s {
+	char userID[100];
+	char password[100];
+	int status;
+	int loginCount;
+} UserInfo;
+
+// Return pointer
+void initializeAccountsFromFile();
+
+// For server
+int userGetAccountIndex(char * username);
+int userCheckPassword(int accIndex, char * input);
+int userCheckLocked(int accIndex);
+void userResetLoginCount(int accIndex);
+void userAddLoginCount(int accIndex);
+
+
+#endif
