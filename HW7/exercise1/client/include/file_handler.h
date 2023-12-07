c2b4485b8dbd93c24067a2f97314fa05b67f9546
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-07 22:02:26 +0700

    Create file_handler.h

---
+++ HW7/exercise1/client/include/file_handler.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+
+void delete(char * file);
+void save(char * data);
+
+#endif
