087f0e72f11b8a47278f9b12a80521497c631116
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-07 22:02:07 +0700

    Create protocol.c

---
+++ HW7/exercise1/server/protocol.c
@@ -0,0 +1,39 @@
+#include "include/protocol.h"
+
+char buffer[BUFFER];
+
+int recvMessage(int sockfd, ServerMessage * msg) {
+    memset(buffer, 0, sizeof(buffer));
+
+    int received = recv(sockfd, buffer, BUFF_MAX + EXTRA_SIZE, 0);
+    if (received < 0) {
+        perror("\nError: ");
+        return -1;
+    } else if (received == 0) {
+        printf("Connection closed.\n");
+        return -1;
+    }
+
+    *msg = SERVER_MESSAGE(buffer);
+    return received;
+}
+
+int sendMessage(int sockfd, ServerMessage * msg) {
+	memset(buffer, 0, sizeof(buffer));
+
+    memcpy(buffer, &msg.opcode, 1); 
+    memcpy(buffer + 1, &msg.length, 2); 
+    memcpy(buffer + 3, msg.payload, BUFFER - 3);
+
+	int bytes_sent = send(sockfd, buffer, msg->length + EXTRA_SIZE, 0);
+	if(bytes_sent < 0) {
+		perror("\nError: ");
+		return -1;
+	}
+
+	return bytes_sent;
+}
+
+void printMsg(ServerMessage * msg) {
+	PRINTF("%d:%d:%s\n", msg->opcode, msg->length, msg->payload);
+}
