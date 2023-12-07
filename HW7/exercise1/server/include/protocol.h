961ca1ef0d678dec065bf1182230dd781de8800c
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-07 22:02:05 +0700

    Create protocol.h

---
+++ HW7/exercise1/server/include/protocol.h
@@ -0,0 +1,26 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#define BUFFER 1024
+
+typedef struct ServerMessage {
+	char opcode; // 1 byte		
+	int length;	// 2 bytes
+	char payload[BUFFER - 3];
+} ServerMessage;
+
+#define SERVER_MESSAGE(str) \
+    {                       \
+        .opcode = (str)[0], \
+        .length = *(int *)((str) + 1), \
+        .payload = {0}, \
+    }
+
+int recvMessage(int sockfd, ServerMessage * msg);
+void printMsg(ServerMessage * msg);
+
+#endif
