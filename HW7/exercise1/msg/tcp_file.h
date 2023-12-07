a57565cd0511d8fa4e37685bb70f50bbac3a4333
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-07 23:22:24 +0700

    Delete tcp_file.h

--- HW7/exercise1/msg/tcp_file.h
+++
@@ -1,7 +0,0 @@
-#ifndef TCP_FILE_H
-#define TCP_FILE_H
-
-void send_file_to_socket(int sockfd, char * filename);
-void recv_file_from_socket(int sockfd, char * filename, int editmode, int key);
-
-#endif
