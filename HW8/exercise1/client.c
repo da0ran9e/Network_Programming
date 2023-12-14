b9ea5c6aed25565a1516d58340a3f6104891b3e5
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-14 11:52:13 +0700

    Update client.c

--- HW8/exercise1/client.c
+++ HW8/exercise1/client.c
@@ -4,16 +4,17 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define MAX_BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s IP_Addr Port_Number\n", argv[0]);
+        printf("Usage: %s IP_Addr Port_Number\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     int clientSocket;
     struct sockaddr_in serverAddr;
+    char inputBuffer[BUFFER_SIZE];
 
     // Create socket
     if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -35,63 +36,18 @@ int main(int argc, char *argv[]) {
     }
 
     while (1) {
-        // Get user input
-        printf("Enter a string (or press Enter to exit): ");
-        char inputBuffer[MAX_BUFFER_SIZE];
-        fgets(inputBuffer, sizeof(inputBuffer), stdin);
-
-        // Remove newline character from input
-        size_t inputLength = strlen(inputBuffer);
-        if (inputBuffer[inputLength - 1] == '\n') {
-            inputBuffer[inputLength - 1] = '\0';
-        }
-
-        // Break the loop if the user enters a blank string
-        if (inputLength == 1 && inputBuffer[0] == '\0') {
+        printf("Enter a string (or press Enter to quit): ");
+        if (fgets(inputBuffer, sizeof(inputBuffer), stdin) == NULL || inputBuffer[0] == '\n') {
             break;
         }
 
-        // Send the input string to the server
-        ssize_t bytesSent = send(clientSocket, inputBuffer, strlen(inputBuffer), 0);
+        // Send the user's input to the server
+        write(clientSocket, inputBuffer, strlen(inputBuffer));
 
-        if (bytesSent == -1) {
-            perror("Error sending data");
-            close(clientSocket);
-            exit(EXIT_FAILURE);
-        }
-
-        // Receive results from the server
-        char resultAlpha[MAX_BUFFER_SIZE];
-        char resultDigit[MAX_BUFFER_SIZE];
-        int undefinedCount;
-
-        struct iovec iov[3];
-        iov[0].iov_base = resultAlpha;
-        iov[0].iov_len = sizeof(resultAlpha);
-        iov[1].iov_base = resultDigit;
-        iov[1].iov_len = sizeof(resultDigit);
-        iov[2].iov_base = &undefinedCount;
-        iov[2].iov_len = sizeof(int);
-
-        ssize_t bytesRead = readv(clientSocket, iov, 3);
-
-        if (bytesRead == -1) {
-            perror("Error receiving results");
-            close(clientSocket);
-            exit(EXIT_FAILURE);
-        }
-
-        // Null-terminate the received data
-        resultAlpha[iov[0].iov_len] = '\0';
-        resultDigit[iov[1].iov_len] = '\0';
-
-        // Print the results
-        printf("Result (Alphabets): %s\n", resultAlpha);
-        printf("Result (Digits): %s\n", resultDigit);
-
-        if (undefinedCount > 0) {
-            printf("Total undefined characters: %d\n", undefinedCount);
-        }
+        // Read and print the results from the server
+        char resultBuffer[BUFFER_SIZE];
+        read(clientSocket, resultBuffer, sizeof(resultBuffer));
+        printf("Server response:\n%s", resultBuffer);
     }
 
     // Close the client socket
