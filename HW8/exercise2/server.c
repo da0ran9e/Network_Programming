01554e02577253f49e9f5f78f927c4198bff934b
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-14 20:01:48 +0700

    Update server.c

--- HW8/exercise2/server.c
+++ HW8/exercise2/server.c
@@ -3,58 +3,65 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 
-#define BUFFER_SIZE 1024
+#define MAX_BUFF_SIZE 1024
+#define QSIZE 10
 
-void handleSignal(int signal) {
-    // Handle signals here if needed
+struct Datagram {
+    void *dg_sa;
+    socklen_t dg_salen;
+    char dg_data[MAX_BUFF_SIZE];
+};
+
+static struct Datagram dg[QSIZE];
+static int iget, iput, nqueue;
+
+void resolve(FILE *out_stream, const char *input) {
+    // Your implementation to resolve IP addresses or domain names
+    // and write the result to the out_stream
 }
 
-void resolveAndSendResult(int clientSocket, const char *input) {
-    struct addrinfo hints, *result, *p;
-    char buffer[BUFFER_SIZE];
+void sig_io(int signo) {
+    struct sockaddr_in cliaddr;
+    socklen_t clilen = sizeof(cliaddr);
+    int n;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
+    dg[iput].dg_salen = clilen;
+    n = recvfrom(STDIN_FILENO, dg[iput].dg_data, MAX_BUFF_SIZE, 0,
+                 (struct sockaddr *)&cliaddr, &dg[iput].dg_salen);
 
-    // Resolve the input (domain name or IP address)
-    if (getaddrinfo(input, NULL, &hints, &result) != 0) {
-        snprintf(buffer, sizeof(buffer), "Not found information\n");
-        sendto(clientSocket, buffer, strlen(buffer), 0, NULL, 0);
-        return;
+    if (n < 0) {
+        perror("recvfrom error");
+        exit(EXIT_FAILURE);
     }
 
-    // Iterate through the list and send the result back to the client
-    for (p = result; p != NULL; p = p->ai_next) {
-        char host[INET6_ADDRSTRLEN];
+    dg[iput].dg_sa = malloc(clilen);
+    memcpy(dg[iput].dg_sa, &cliaddr, clilen);
+    if (++iput >= QSIZE)
+        iput = 0;
 
-        if (getnameinfo(p->ai_addr, p->ai_addrlen, host, sizeof(host), NULL, 0, NI_NUMERICHOST) != 0) {
-            perror("getnameinfo failed");
-            continue;
-        }
-
-        snprintf(buffer, sizeof(buffer), "%s\n", host);
-        sendto(clientSocket, buffer, strlen(buffer), 0, NULL, 0);
+    if (iput == iget) {
+        fprintf(stderr, "receive buffer overflow\n");
+        exit(EXIT_FAILURE);
     }
 
-    freeaddrinfo(result);
+    nqueue++;
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s Port_Number\n", argv[0]);
+        fprintf(stderr, "Usage: %s PortNumber\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int serverSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrLen = sizeof(clientAddr);
+    int sockfd;
+    struct sockaddr_in serverAddr;
 
     // Create socket
-    if ((serverSocket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -66,37 +73,47 @@ int main(int argc, char *argv[]) {
     serverAddr.sin_port = htons(atoi(argv[1]));
 
     // Bind the socket to the specified port
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+    if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
         perror("Bind failed");
-        close(serverSocket);
+        close(sockfd);
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d...\n", atoi(argv[0]));
+    int on = 1;
+    sigset_t zeromask, newmask, oldmask;
+    
+    sigemptyset(&zeromask);
+    sigemptyset(&oldmask);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGIO);
 
-    // Register signal handler
-    signal(SIGINT, handleSignal);
+    signal(SIGIO, sig_io);
+    fcntl(sockfd, F_SETOWN, getpid());
+    ioctl(sockfd, FIOASYNC, &on);
+    ioctl(sockfd, FIONBIO, &on);
+
+    sigprocmask(SIG_BLOCK, &newmask, &oldmask);
 
     while (1) {
-        char buffer[BUFFER_SIZE];
-        ssize_t bytesRead;
+        while (nqueue == 0)
+            sigsuspend(&zeromask);
 
-        // Receive data from the client
-        bytesRead = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &addrLen);
+        sigprocmask(SIG_SETMASK, &oldmask, NULL);
 
-        if (bytesRead <= 0) {
-            perror("Error receiving data from client");
-            continue;
-        }
+        char result[MAX_BUFF_SIZE];
+        FILE *out_stream = fmemopen(result, MAX_BUFF_SIZE - 1, "w");
+        resolve(out_stream, dg[iget].dg_data);
+        fclose(out_stream);
 
-        buffer[bytesRead] = '\0';  // Null-terminate the received data
+        sendto(sockfd, result, MAX_BUFF_SIZE, 0,
+               dg[iget].dg_sa, dg[iget].dg_salen);
 
-        // Process client request and send the result
-        resolveAndSendResult(serverSocket, buffer);
+        if (++iget >= QSIZE)
+            iget = 0;
+
+        sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+        nqueue--;
     }
-
-    // Close the server socket
-    close(serverSocket);
 
     return 0;
 }
