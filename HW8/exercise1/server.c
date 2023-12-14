d0f0f48e54b41085d58283e4320ccc988da467c7
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-14 11:50:07 +0700

    Update server.c

--- HW8/exercise1/server.c
+++ HW8/exercise1/server.c
@@ -2,29 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <ctype.h>
 #include <arpa/inet.h>
+#include <ctype.h>
 
 #define MAX_BUFFER_SIZE 1024
-
-void processString(const char *input, char *alphaString, char *digitString, int *undefinedCount) {
-    int alphaIndex = 0, digitIndex = 0;
-    *undefinedCount = 0;
-
-    for (size_t i = 0; i < strlen(input); i++) {
-        char ch = input[i];
-        if (isalpha(ch)) {
-            alphaString[alphaIndex++] = ch;
-        } else if (isdigit(ch)) {
-            digitString[digitIndex++] = ch;
-        } else {
-            (*undefinedCount)++;
-        }
-    }
-
-    alphaString[alphaIndex] = '\0';
-    digitString[digitIndex] = '\0';
-}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -32,84 +13,94 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int port = atoi(argv[1]);
-    if (port <= 0 || port > 65535) {
-        fprintf(stderr, "Invalid port number\n");
-        exit(EXIT_FAILURE);
-    }
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
 
     // Create socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
     // Set up server address struct
-    struct sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
+    serverAddr.sin_port = htons(atoi(argv[1]));
 
     // Bind the socket to the specified port
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
         perror("Bind failed");
         close(serverSocket);
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 1) == -1) {
+    if (listen(serverSocket, 5) == -1) {
         perror("Listen failed");
         close(serverSocket);
         exit(EXIT_FAILURE);
     }
 
-    printf("Server started. Listening on port %d...\n", port);
-
-    // Accept incoming connections
-    int clientSocket = accept(serverSocket, NULL, NULL);
-    if (clientSocket == -1) {
-        perror("Accept failed");
-        close(serverSocket);
-        exit(EXIT_FAILURE);
-    }
-
-    char inputBuffer[MAX_BUFFER_SIZE];
-    char alphaBuffer[MAX_BUFFER_SIZE];
-    char digitBuffer[MAX_BUFFER_SIZE];
-    int undefinedCount;
+    printf("Server listening on port %d...\n", atoi(argv[1]));
 
     while (1) {
-        // Receive a string from the client
-        ssize_t bytesRead = recv(clientSocket, inputBuffer, sizeof(inputBuffer), 0);
+        // Accept a connection
+        if ((clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen)) == -1) {
+            perror("Accept failed");
+            continue;
+        }
+
+        printf("Connection accepted from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+        // Receive the string from the client
+        char buffer[MAX_BUFFER_SIZE];
+        ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
 
         if (bytesRead <= 0) {
-            break; // Connection closed or error
+            perror("Error receiving data");
+            close(clientSocket);
+            continue;
         }
 
-        inputBuffer[bytesRead] = '\0';
+        // Process the received string
+        buffer[bytesRead] = '\0';
 
-        if (strcmp(inputBuffer, "") == 0) {
-            break; // Break the loop on a blank string
+        char resultAlpha[MAX_BUFFER_SIZE] = "";
+        char resultDigit[MAX_BUFFER_SIZE] = "";
+        int undefinedCount = 0;
+
+        for (size_t i = 0; i < bytesRead; i++) {
+            if (isalpha(buffer[i])) {
+                strncat(resultAlpha, &buffer[i], 1);
+            } else if (isdigit(buffer[i])) {
+                strncat(resultDigit, &buffer[i], 1);
+            } else {
+                undefinedCount++;
+            }
         }
-
-        // Process the string
-        processString(inputBuffer, alphaBuffer, digitBuffer, &undefinedCount);
 
         // Send the results back to the client
-        send(clientSocket, alphaBuffer, strlen(alphaBuffer), 0);
-        send(clientSocket, digitBuffer, strlen(digitBuffer), 0);
+        struct iovec iov[3];
+        iov[0].iov_base = resultAlpha;
+        iov[0].iov_len = strlen(resultAlpha);
+        iov[1].iov_base = resultDigit;
+        iov[1].iov_len = strlen(resultDigit);
+        iov[2].iov_base = &undefinedCount;
+        iov[2].iov_len = sizeof(int);
 
-        // Send the count of undefined characters
-        char undefinedCountStr[10];
-        snprintf(undefinedCountStr, sizeof(undefinedCountStr), "%d", undefinedCount);
-        send(clientSocket, undefinedCountStr, strlen(undefinedCountStr), 0);
+        ssize_t bytesSent = writev(clientSocket, iov, 3);
+
+        if (bytesSent == -1) {
+            perror("Error sending results");
+        }
+
+        // Close the client socket
+        close(clientSocket);
     }
 
-    // Close sockets
-    close(clientSocket);
+    // Close the server socket
     close(serverSocket);
 
     return 0;
